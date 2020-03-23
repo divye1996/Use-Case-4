e8746263a8b414973db92d6aaac9fbc269f0c8e9
god <god@DESKTOP-0NN631V>
2020-03-23 10:25:18 +0530

    Commit 1

---
+++ docker-spring-boot/src/test/java/com/dk/docker/dockerspringboot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.docker.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
